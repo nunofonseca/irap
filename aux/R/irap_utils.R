4c6639ba562a4a9a0e56c3fac31b86da63fd145c
Nuno Fonseca <firstname.lastname@gmail.com>
2013-09-10 10:21:49 +0100

    Updated version
    Former-commit-id: 63f017240efbb81b0d99d6259d5aa363311a5e8b [formerly 2a295dc53e1deb429260b578529caa9be5a60cdc]
    Former-commit-id: 0e87cac8e7c316cb9f81fb344b6a4a9f01325733

--- aux/R/irap_utils.R
+++ aux/R/irap_utils.R
@@ -20,7 +20,7 @@
 # =========================================================
 # TODO: create an object for the conf. information
 
-irap_version<-"0.2.0"
+irap_version<-"0.2.2"
 
 IRAP.DIR <- Sys.getenv(c("IRAP_DIR"))
 if ( IRAP.DIR == "" ) {
--- src/bamutils/bam_fix_se_flag.c
+++ src/bamutils/bam_fix_se_flag.c
@@ -30,7 +30,7 @@
 #include <string.h>
 #include <assert.h>
 
-#define VERSION "0.2.0"
+#define VERSION "0.2.2"
 
 
 #define BACKLINE "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
