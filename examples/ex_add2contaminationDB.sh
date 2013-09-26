5529635cb8048eacbe56fbf3fb7df4d3733bd028
Nuno Fonseca <firstname.lastname@gmail.com>
2013-09-26 10:07:10 +0100

    Example: Creation of a contamination DB
    Former-commit-id: a49c32cadbfeac500cc56acd04f53be8f3941112 [formerly c8c4c915367057e6f435027304b05b2bb668e4ae]
    Former-commit-id: c46f4ac828661d18d0e16d871f36acd63bf5ac7e

---
+++ examples/ex_add2contaminationDB.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Create a fungi and microbial contamination DB
+set -e
+wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/fungi/fungi.16.1.genomic.fna.gz
+#wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/microbial/microbial.69.1.genomic.fna.gz
+
+gunzip fungi.16.1.genomic.fna.gz
+gunzip microbial.69.1.genomic.fna.gz
+
+cat fungi.16.1.genomic.fna microbial.69.1.genomic.fna > combined.fa
+irap_add2contaminationDB -n fungi_16_1_microbial_99_1 -f combined.fa
+rm -f fungi.16.1.genomic.fna microbial.69.1.genomic.fna combined.fa
+
+echo "IRAP: cont_index=fungi_16_1_microbial_99_1"
+exit 0
