f2ae6fcc858f352d0d9cab593066308d4afe8497
Nuno Fonseca <firstname.lastname@gmail.com>
2013-12-13 16:20:46 +0000

    Atlas related targets
    Former-commit-id: d83c09712e29a4af3b04c4b3354d55ea7b4364f8 [formerly e72160406d9abd5fe159e24a8cd6a93322049c60]
    Former-commit-id: 2e94b8106bdc276ee5741cdbc7cda6c477d217b4

---
+++ scripts/atlas_clean_html.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [ "$1-" = "-" ] ; then
+    echo "atlas_clean_html.sh HTML file" > /dev/stderr
+    exit 1
+fi
+
+if [ ! -e "$1" ] ; then
+    echo "ERROR: file not found $1" > /dev/stderr
+    exit 1
+fi
+
+# remove header
+LINE=`grep -n -i -F "<body" $1 | cut -f 1 -d:`
+if [ $? != 0 ] || [ "$LINE-" = "-" ]; then
+    echo "ERROR: Unable to find body in $1"
+    exit 1
+fi
+HLINE=`expr $LINE + 1`
+FLINE=`grep -n -i -F "</body" $1 | cut -f 1 -d:`
+if [ $? != 0 ]|| [ "$FLINE-" = "-" ]; then
+    echo "ERROR: Unable to find </body> in $1"
+    exit 1
+fi
+# footer
+FLINE=`expr $FLINE - 1`
+set -e
+head -n +$FLINE $1 | tail -n +$HLINE | grep -v -F "<nav>  <ul>   <li><a href='info.html' >Info</a>" > $1.tmp
+mv $1.tmp $1
+exit 0
