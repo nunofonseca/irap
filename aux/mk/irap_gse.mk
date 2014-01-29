f91ca0821ed6ff76483a0926a7f88fdd306e7dda
Nuno Fonseca <firstname.lastname@gmail.com>
2014-01-29 00:30:59 +0000

    Initial support for GSE analysis
    Former-commit-id: 3b7214eac5eb89f0bc08cffa9a26fc6f503ebc15 [formerly d77b4e19f817400e9c6598a8fe583fcc071d4e30]
    Former-commit-id: c152503caf77780fb31f2676eab32db3247a196a

---
+++ aux/mk/irap_gse.mk
@@ -0,0 +1,81 @@
+#; -*- mode: Makefile;-*-
+# =========================================================
+# Copyright 2012-2013,  Nuno A. Fonseca (nuno dot fonseca at gmail dot com)
+#
+# This file is part of iRAP.
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with iRAP.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    $Id: 0.1.3 Nuno Fonseca Wed Dec 26 16:16:19 2012$
+# =========================================================
+
+#************
+# GSE
+#************
+
+# Currently only one tool is supported
+
+# default values
+gse_tool:=none
+gse_method:=fisher
+
+
+gse_pvalue=0.05
+# minimum number of genes in a set
+gse_minsize=3
+
+
+
+gse_valid_tools=piano none
+ifeq (,$(filter $(gse_tool),$(gse_valid_tools)))
+$(call p_info,[ERROR] gse_tool)
+$(error $(gse_tool) not supported)
+endif
+
+################################
+# Piano
+
+ifeq ($(gse_tool),piano)
+
+define run_piano_goterm=
+irap_GSE_piano --tsv $1 --out $(subst .tsv,,$2) --annotation $(annot_tsv) --pvalue $(gse_pvalue) --minsize $(gse_minsize) --method $(gse_method) 
+endef
+
+define run_piano_kegg=
+irap_GSE_piano --tsv $1 --out $(subst .tsv,,$2) --annotation $(annot_tsv) --pvalue $(gse_pvalue) --minsize $(gse_minsize) --method $(gse_method) 
+endef
+
+################################
+# validate the options
+gse_piano_valid_methods=mean median sum fisher stouffer tailStrength
+ifeq (,$(filter $(gse_method),$(gse_piano_valid_methods)))
+$(call p_info,[ERROR] gse_method)
+$(error $(gse_method) not supported)
+endif
+
+
+$(name)/$(mapper)/$(quant_method)/$(de_method)/%.$(gse_tool).$(gse_method).go.tsv: $(name)/$(mapper)/$(quant_method)/$(de_method)/%_de.tsv $(annot_tsv)
+	$(call run_piano_goterm,$<,$@)
+
+$(name)/$(mapper)/$(quant_method)/$(de_method)/%.$(gse_tool).$(gse_method).kegg.tsv: $(name)/$(mapper)/$(quant_method)/$(de_method)/%_de.tsv $(annot_tsv)
+	$(call run_piano_kegg,$<,$@)
+
+else
+# nothing to do
+gse_stage:  
+
+endif
+
+phony_targets+= gse_stage
