9de058e6f36505854187c8f3a4a2b59af5bbc86a
Nuno Fonseca <firstname.lastname@gmail.com>
2014-02-15 22:18:59 +0000

    Collect info about the packages installed in R verson 3
    Former-commit-id: 6d432f5c0e5a34082baa5f920b0de6ff64efcba7 [formerly 1b7bb6898fad8a906d421fdf2d762566c20feb2b]
    Former-commit-id: be24039e15610044f8c1fb8438b0266607f94c1e

--- scripts/irap_gen_version_mk.sh
+++ scripts/irap_gen_version_mk.sh
@@ -7,7 +7,8 @@ DEST_FILE=$IRAP_DIR/aux/mk/irap_versions.mk
 echo  "# Generated `date` " > $DEST_FILE
 grep "VERSION=" $IRAP_DIR/scripts/irap_install.sh |sort -u | grep -v "^#"| sed -e "s/^ *//" >> $DEST_FILE
 # R packages
-irap_R_package_version.R DESeq edgeR  Rsamtools DEXSeq baySeq  >> $DEST_FILE
+irap_R_package_version.R DESeq edgeR Rsamtools limma DEXSeq baySeq >> $DEST_FILE
+irap_R3_package_version.R piano  >> $DEST_FILE
 
 sed -i "s/VERSION=/version=/" $DEST_FILE
 exit 0
