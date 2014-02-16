dcda8a445347aa4dbc659b5e389f45d57e76484e
Nuno Fonseca <firstname.lastname@gmail.com>
2014-02-16 22:42:51 +0000

    More GSE related changes
    Former-commit-id: 64c8ba2dcb19558b36788b782a9219da76248b99 [formerly ea62cc591d3b09144e3d450aab6651ae0a6072ae]
    Former-commit-id: 20ab66ed6d789e1bc22b4a2f078316ed62affae0

--- aux/mk/irap_report.mk
+++ aux/mk/irap_report.mk
@@ -314,8 +314,15 @@ $(name)/report/%.genes_de.html: $(name)/%.genes_de.tsv $(annot_tsv)
 phony_targets+=gse_report gse_report_files
 silent_targets+=gse_report_files
 
+# only generates the html iff the respective GSE tsv file exists
+
 gse_report: report_setup $(call gse_html_files,$(name))
 # just print the name of the files that will be produced
 gse_report_files:
 	echo $(call gse_html_files,$(name))
 
+
+############################
+# all targets
+phony_targets+= report_all_targets
+report_all_targets:  $(name)/report/index.html qc_report mapping_report quant_report de_report gse_report show_citations end_report
