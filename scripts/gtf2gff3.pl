ef6ab903cfb4729bca453fe224c911e2922e401f
Nuno Fonseca <firstname.lastname@gmail.com>
2014-03-13 11:00:04 +0000

    Ignore comments in GTF file
    Former-commit-id: 4d2e9200706e6517a767796fa170d734d7736f77 [formerly a69c909ef3245543c7cea4d5265e06d9710d41d1]
    Former-commit-id: 15aeb55e69afdacd0c77321b545f45b3ded73d3d

--- scripts/gtf2gff3.pl
+++ scripts/gtf2gff3.pl
@@ -12,10 +12,11 @@ my @trs=();
 my $counter = 0;
 
 while (<>) {
+        next if ( /^#/ ); # ignore comments
 	@v = split /\t/;
 	next unless $v[2] eq 'exon';
 	$v[0] = 'chr' . $v[0] unless index($v[0], 'chr') == 0;
-	($g,$tr) = $v[8] =~ /gene_id "([^"]+)"; transcript_id "([^"]+)/;
+	($g,$tr) = $v[8] =~ /gene_id "([^"]+)";.* transcript_id "([^"]+)/;
 	if ($tr ne $last_tr) {
 		push @trs, [@exons];
 		if ($g ne $last_g) {
