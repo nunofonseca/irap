f28ba6877d3176383bddf1b99f20925ce2568a72
Nuno Fonseca <firstname.lastname@gmail.com>
2014-03-13 00:31:21 +0000

    Update version to 0.4.0d1
    Former-commit-id: 9669e2e5a35871ea7524f6f45f5800f465ba21f7 [formerly 729d5597279af1326ed49710f0d488769ab28b05]
    Former-commit-id: 95405d290039c5c8f52185ae78c7a0d3e845ea51

--- aux/R/irap_utils.R
+++ aux/R/irap_utils.R
@@ -20,7 +20,7 @@
 # =========================================================
 # TODO: create an object for the conf. information
 
-irap_version<-"0.4.0-devel"
+irap_version<-"0.4.0d1"
 
 IRAP.DIR <- Sys.getenv(c("IRAP_DIR"))
 if ( IRAP.DIR == "" ) {
