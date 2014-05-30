63a4f36f317d853822975875b3a128bc9e20ece6
Nuno Fonseca <firstname.lastname@gmail.com>
2014-05-30 11:10:27 +0100

    Update version to 0.4.3d
    Former-commit-id: d1cd89769236801fa07a6d4cde59801199d57386 [formerly fedb9f1d3a3d75be8e721d38592c4884d5cb7ee8]
    Former-commit-id: b3795b1f2914284d7fbb721cd76abaa9ea99e4cd

--- src/bamutils/bam_fix_NH.c
+++ src/bamutils/bam_fix_NH.c
@@ -33,7 +33,7 @@
 
 #define HASHSIZE 7000001
 // Same version as IRAP
-#define VERSION "0.4.2d"
+#define VERSION "0.4.3d"
 struct read {
   uint8_t ctr; // how many times a read appears in one alignment
   char *name;// read name
--- src/bamutils/bam_fix_se_flag.c
+++ src/bamutils/bam_fix_se_flag.c
@@ -30,7 +30,7 @@
 #include <string.h>
 #include <assert.h>
 
-#define VERSION "0.4.2d"
+#define VERSION "0.4.3d"
 
 
 #define BACKLINE "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
