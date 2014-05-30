9e76013a22f31581ce2cf930fe9199f2e75f8abe
Nuno Fonseca <firstname.lastname@gmail.com>
2014-05-30 11:18:55 +0100

    removing realpath since it is not widely available :(
    Former-commit-id: fcbc09b92d8a07b4893b588d1a5e57240c23abc6 [formerly aa335b8d660695b42b75dd3ab73cf8f46ec7557d]
    Former-commit-id: 253b4556d033cf2628fbbcfc624fa8139cdcb85e

--- scripts/irap_install.sh
+++ scripts/irap_install.sh
@@ -1666,7 +1666,7 @@ check_dependencies
 # Full path
 pinfo "Checking paths..."
 #IRAP_DIR=$(realpath -f "$IRAP_DIR")
-IRAP_DIR=$(reallink -f "$IRAP_DIR")
+IRAP_DIR=$(readlink -f "$IRAP_DIR")
 SRC_DIR=$(readlink -f "$SRC_DIR")
 pinfo "IRAP_DIR=$IRAP_DIR"
 pinfo "SRC_DIR=$SRC_DIR"
