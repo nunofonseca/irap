59bb10f0ff66b24f7e9da844c0f5d903507625f2
Nuno Fonseca <firstname.lastname@gmail.com>
2014-06-14 12:11:17 +0100

    TODO: generation of the status.html
    Former-commit-id: 5d29225a23543d43b240b0f4f80f34c7f3718441 [formerly 0ee2b8e223828ce257aedb3ce4947a27eeecaca3]
    Former-commit-id: 44cebee47ff97343a885e489e46b18b670490e17

--- aux/mk/irap_report.mk
+++ aux/mk/irap_report.mk
@@ -431,7 +431,8 @@ end_report: $(name)/report/index.html $(call must_exist,$(name)/report/irap.css)
 
 
 # TODO: replace versions.html by info_report
-$(name)/report/index.html: $(conf) $(info_targets)  $(quant_html_files) $(qc_html_files) $(mapping_report_targets) $(call de_html_files,$(name)) $(call gse_html_files,$(name))  $(call must_exist,$(name)/report/jbrowse/index.html) $(call must_exist,$(name)/report/status.html) $(name)/report/about.html $(call must_exist,$(name)/report/irap.css) $(call must_exist,$(name)/report/menu.css)
+# TODO $(call must_exist,$(name)/report/status.html)a
+$(name)/report/index.html: $(conf) $(info_targets)  $(quant_html_files) $(qc_html_files) $(mapping_report_targets) $(call de_html_files,$(name)) $(call gse_html_files,$(name))  $(call must_exist,$(name)/report/jbrowse/index.html)  $(name)/report/about.html $(call must_exist,$(name)/report/irap.css) $(call must_exist,$(name)/report/menu.css)
 	cp  $(name)/report/info.html $@ &&
 	irap_report_main $(IRAP_REPORT_MAIN_OPTIONS) --conf $(conf) --rep_dir $(name)/report -m "$(call mapping_dirs,$(name))" -q "$(call quant_dirs,$(name))" -d "$(call de_dirs,$(name))" &&
 	touch $@
