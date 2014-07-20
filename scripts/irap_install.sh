90aebeffaba2834be7a8566c2bf6e4eec12acc04
Nuno Fonseca <firstname.lastname@gmail.com>
2014-07-20 20:34:00 +0100

    Upgrade bowtie 1 and 2
    Former-commit-id: 020593884ced3cf2bd81bb0514b0e0ca6cae2e18 [formerly 576965bd90cd5af3a795b3add65549295241c07c]
    Former-commit-id: 4f588cbcb97f0f99a78751496977b0a3b0ebc9e6

--- scripts/irap_install.sh
+++ scripts/irap_install.sh
@@ -140,11 +140,11 @@ BFAST_VERSION=0.7.0a
 BFAST_FILE=bfast-$BFAST_VERSION.tar.gz
 BFAST_URL=http://sourceforge.net/projects/bfast/files/bfast/0.7.0/$BFAST_FILE
 #
-bowtie1_VERSION=1.0.0
+bowtie1_VERSION=1.0.1
 bowtie1_FILE=bowtie-${bowtie1_VERSION}-linux-x86_64.zip
 bowtie1_URL=http://sourceforge.net/projects/bowtie-bio/files/bowtie/$bowtie1_VERSION/$bowtie1_FILE
 #
-bowtie2_VERSION=2.1.0
+bowtie2_VERSION=2.2.3
 bowtie2_FILE=bowtie2-${bowtie2_VERSION}-linux-x86_64.zip
 bowtie2_URL=http://sourceforge.net/projects/bowtie-bio/files/bowtie2/$bowtie2_VERSION/$bowtie2_FILE
 
