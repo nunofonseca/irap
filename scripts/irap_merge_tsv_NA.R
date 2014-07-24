ac2cf5202532e632488708067fab4cfaccba7f7f
Nuno Fonseca <firstname.lastname@gmail.com>
2014-07-24 15:39:03 +0100

    Read the cmd line options from stdin if necessary
    Former-commit-id: da0fe824f0372fea36521d86ad9883c3c6332770 [formerly 383c5b4afccd54b047477584dd5fb50764ad8480]
    Former-commit-id: 985fe743cc7ca9105d951987f497abbfae133d7a

--- scripts/irap_merge_tsv_NA.R
+++ scripts/irap_merge_tsv_NA.R
@@ -1,5 +1,4 @@
 #!/usr/bin/env Rscript
-args <- commandArgs(trailingOnly=TRUE)
 
 IRAP.DIR <- Sys.getenv(c("IRAP_DIR"))
 if ( IRAP.DIR == "" ) {
@@ -8,6 +7,9 @@ if ( IRAP.DIR == "" ) {
 }
 source(paste(IRAP.DIR,"aux/R","irap_utils.R",sep="/"))
 
+
+#args <- commandArgs(trailingOnly=TRUE)
+args <- importArgsfromStdin()
 
 if (length(args)<1) {
   cat("ERROR! usage: irap_merge_tsv_NA.R  file1.tsv [file2.tsv file3.tsv ...]\n")
@@ -18,11 +20,19 @@ if (length(args)<1) {
 
 files <- args
 f1<-files[1]
+if (!file.exists(f1)) {
+  perror("Unable to find file ",f1)
+  q(status=1)
+}
 files<-files[-1]
 t1<-read.table(f1,sep="\t",as.is=c(T,F),comment.char="",quote="\"")
 t1 <- fix.cufflinks.fpkms(t1)
 colnames(t1)<-c("Gene",basename(f1))
 for ( f in  files ) {
+        if (!file.exists(f)) {
+          perror("Unable to find file ",f)
+          q(status=1)
+        }
 	t2<-read.table(f,sep="\t",as.is=c(T,F),comment.char="",quote="\"")
         t2 <- fix.cufflinks.fpkms(t2)
         colnames(t2) <- c("Gene",basename(f))
--- scripts/irap_merge_tsv_NA.sh
+++ scripts/irap_merge_tsv_NA.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # $Id: 0.1.3 Nuno Fonseca Fri Dec 21 14:59:29 2012$
-# Merge TSV files by the first column 
+# Merge TSV files using the first column 
 FILES=$*
 set -e
-irap_merge_tsv_NA.R $FILES  > $1.tmp
+if [ "$1-" == "-stdin-" ]; then
+    cat | irap_merge_tsv_NA.R -stdin > $1.tmp
+else
+    irap_merge_tsv_NA.R $FILES  > $1.tmp
+fi
 sed  -e "s/\.[^\.]*\.tsv//g;s/\(.raw\|.rpkms\|.genes\|.nlib\|.exons\|.transcripts\)//g;s/\(\.pe\|\.se\)//g" $1.tmp
 rm -f $1.tmp
 exit 0
