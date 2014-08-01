dcdc0c524a1e1745677ea04990b6ea5cf8b20361
Nuno Fonseca <firstname.lastname@gmail.com>
2014-08-01 11:21:39 +0100

    Use fastq_num_reads
    Former-commit-id: fcaac12f76e713d4c82bb54cf372bb7a8a54ecd4 [formerly 914f76ab8d1e182eab729031a4c40c257fdf345d]
    Former-commit-id: 904b90890b120af55ba4eea973ac732726ce88fc

--- scripts/num_reads.sh
+++ scripts/num_reads.sh
@@ -5,10 +5,14 @@ if [ "-$1" = "-" ]; then
 fi
 
 if [ -e $1 ]; then 
-    grep -c "^+$" $1
+    fastq_num_reads $1
 else 
+    echo "ERROR! $1 file not found" >&2
+    exit 1
 if [ -e $1.gz ]; then 
-    zgrep -c "^+$" $1.gz
+    echo "ERROR! .gz files are not supported" >&2
+    exit 1
+#    zgrep -c "^+$" $1.gz
 else
     echo "ERROR! File $1(.gz) not found." >&2
     exit 1
