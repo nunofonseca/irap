c5b9ff83eb763b89e55277b2ea3a30d9a7eb505d
Nuno Fonseca <firstname.lastname@gmail.com>
2014-08-17 21:51:16 +0100

    Add Atlas run mode
    Former-commit-id: dc8f247af6705b6ca8002b8e518f088f05cd6c40 [formerly df2d7ae5b9bf580dd1e01591486d89c79c29364a]
    Former-commit-id: fcb5678d3f134fd9d9c4ce52ec3f011b4c77c607

---
+++ aux/mk/irap_atlas_init.mk
@@ -0,0 +1,29 @@
+#; -*- mode: Makefile;-*-
+# =========================================================
+# Copyright 2012-2014,  Nuno A. Fonseca (nuno dot fonseca at gmail dot com)
+#
+# This file is part of iRAP.
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with iRAP.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    $Id$
+# =========================================================
+# Code executed after reading the configuration file and before validating the options
+# It should be used to override default options
+
+# $(lib)_dir default dir should be $(exp_name)_$(species)
+def_lib_dir=$(name)_$(species)
+
+# Options
