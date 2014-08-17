d7c34eef5124c83035d18ff07a889fdafaca2c90
Nuno Fonseca <firstname.lastname@gmail.com>
2014-08-17 22:34:29 +0100

    Update version to 0.4.3d2
    Former-commit-id: 78c81876120afc74bb5ab02068833868f4c844b8 [formerly a2bd55e457cb35a2b618392c020e0e40b2b98cc5]
    Former-commit-id: 72e36b7fa6a7ba98559e99ae6470de4931225188

--- src/bamutils/bam_fix_se_flag.c
+++ src/bamutils/bam_fix_se_flag.c
@@ -30,7 +30,7 @@
 #include <string.h>
 #include <assert.h>
 
-#define VERSION "0.4.3d1"
+#define VERSION "0.4.3d2"
 
 
 #define BACKLINE "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
--- src/bamutils/bam_pe_insert.c
+++ src/bamutils/bam_pe_insert.c
@@ -27,7 +27,7 @@
 #include <sam.h>
 #include <kstring.h>      
 
-#define VERSION "0.4.3d1"
+#define VERSION "0.4.3d2"
 #define MAX_INSERT_SIZE 1000
 #define BIN_SIZE 10
      
