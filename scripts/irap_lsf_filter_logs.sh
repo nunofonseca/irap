f818ec0351ecf3b097be38a6100af74ee82e0d6e
Nuno Fonseca <firstname.lastname@gmail.com>
2014-09-01 14:37:26 +0100

    Improve error reporting in irap_*lsf
    Former-commit-id: 5680595ede1bff2f646467b6bd453b8dbbf45db2 [formerly 3b06b0f03ee0295ffda63516219ea4bd7d0ae2d9]
    Former-commit-id: d13e47a5a85026d25ee56243f737210d43c01ac9

---
+++ scripts/irap_lsf_filter_logs.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+if [ "$1-" == "-" ]; then
+    echo "Usage: irap_lsf_filter_logs log_dir" > /dev/stderr
+    exit 1
+fi
+
+INSUF_MEM=0
+# Provide a bit more information
+function check_jobs_status {
+  dir=$1
+  let success_jobs=0
+  let failed_jobs=0
+
+  function check_jobs_in_dir {
+    out_files=`ls -1 $dir/*.out`
+    for f in $out_files; do
+       OK=`grep -c -E  "^Successfully completed." $f`
+       #echo "OK=$OK" > /dev/stderr
+       if [ "$OK" == "1" ]; then
+         let success_jobs=$success_jobs+1
+       else 
+         # error
+         LAST_LINES=`tail -n 50 $f|head -n 10`
+         let failed_jobs=$failed_jobs+1
+         EXIT_STATUS=`grep "Exited with exit code" $f|tail -n 1 |cut -f 5 -d\ |sed "s/\.$//"`
+         CMD=`grep "LSBATCH: User input" -A 1 $f|tail -n 1`
+	 if [ "$EXIT_STATUS" == "1" ]; then 
+	     INSUF_MEM=1
+	 fi
+	 echo "------------------------------------------" > /dev/stderr
+	 echo "Out file: $f" > /dev/stderr
+         echo "Exit status: $EXIT_STATUS" > /dev/stderr
+	 echo "$CMD" > /dev/stderr
+	 echo "$LAST_LINES" > /dev/stderr
+    fi
+    done
+    # checking subfolders
+    local DIRS=`ls -d $dir/*/ 2> /dev/null`
+    local d
+    for d in $DIRS; do
+      check_jobs_in_dir $d
+    done 
+   }
+   check_jobs_in_dir $dir
+   
+   echo JOBS OK=$success_jobs
+   if [ $failed_jobs \> 0 ]; then
+       echo JOBS FAILED=$failed_jobs
+       if [ $INSUF_MEM == 1 ]; then 
+	   EXIT_STATUS=1
+       else
+	   EXIT_STATUS=2
+       fi
+   else
+       EXIT_STATUS=0
+   fi
+   
+}
+check_jobs_status $1
+exit $EXIT_STATUS
