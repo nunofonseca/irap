73880f8bc4298e68e3c7ee2326f2cd187bbf4f42
Nuno Fonseca <firstname.lastname@gmail.com>
2014-10-06 16:07:07 +0100

    print NA if fastq file is not found
    Former-commit-id: 90bdc698ed61b46519262cb11148caf2787c4f5f [formerly 83a724d30a85b73fad84963ed1cec3c15167d295]
    Former-commit-id: d4ad052090e7b6bbb583bbaec93c635721a8bba7

--- scripts/num_reads.sh
+++ scripts/num_reads.sh
@@ -8,7 +8,8 @@ if [ -e $1 ]; then
     fastq_num_reads $1
 else 
     echo "ERROR! $1 file not found" >&2
-    exit 1
+    echo NA
+    exit 0
 if [ -e $1.gz ]; then 
     echo "ERROR! .gz files are not supported" >&2
     exit 1
