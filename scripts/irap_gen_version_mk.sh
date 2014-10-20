ef4ddcd2ae3e5d611d9b91be8560af218896e111
Nuno Fonseca <firstname.lastname@gmail.com>
2014-10-20 16:40:30 +0100

    do not crash if R is not installed
    Former-commit-id: 3727f1a6935ae8d48aea99a3077dee210bf55dbb [formerly dc4cf49620862c118d3a99e92395adf3cd46a822]
    Former-commit-id: ab315fa0909a3705b75529637aeeff55588f10e8

--- scripts/irap_gen_version_mk.sh
+++ scripts/irap_gen_version_mk.sh
@@ -7,7 +7,11 @@ DEST_FILE=$IRAP_DIR/aux/mk/irap_versions.mk
 echo  "# Generated `date` " > $DEST_FILE
 grep "VERSION=" $IRAP_DIR/scripts/irap_install.sh |sort -u | grep -v "^#"| sed -e "s/^ *//" >> $DEST_FILE
 # R packages
-irap_R_package_version.R DESeq edgeR Rsamtools limma DEXSeq baySeq >> $DEST_FILE
+if [ "`which R >/dev/null`-" = "-" ]; then
+    echo "R not found" > /dev/stderr
+else
+    irap_R_package_version.R DESeq edgeR Rsamtools limma DEXSeq baySeq >> $DEST_FILE
+fi
 
 if [ -x $IRAP_DIR/scripts/R3 ]; then
     irap_R3_package_version.R piano  >> $DEST_FILE
