f21669138456c74bdd255c299e7b16563b898f65
Nuno Fonseca <firstname.lastname@gmail.com>
2014-10-27 11:55:09 +0000

    Update version to 0.5.0p2
    Former-commit-id: 1ff1fdd80fafea240349457f05d703eda2dbc776 [formerly 2b1c0bced820552d4abd26dc92b4aa2b6f516f2d]
    Former-commit-id: ffb985fd8bf339f75255f418f248ab5d6f9b9d2f

--- scripts/irap_install.sh
+++ scripts/irap_install.sh
@@ -23,7 +23,7 @@
 install=all
 IRAP_DIR1=
 SRC_DIR=
-IRAP_VERSION=0.5.1.d1
+IRAP_VERSION=0.5.0p2
 
 #
 USE_CACHE=y
--- src/bamutils/bam_fix_se_flag.c
+++ src/bamutils/bam_fix_se_flag.c
@@ -30,7 +30,7 @@
 #include <string.h>
 #include <assert.h>
 
-#define VERSION "0.5.1.d1"
+#define VERSION "0.5.0p2"
 
 
 #define BACKLINE "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
