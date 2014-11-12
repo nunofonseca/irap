af31394e6e74d6a24da19725e727212d47420490
Nuno Fonseca <firstname.lastname@gmail.com>
2014-11-12 11:41:28 +0000

    Make upgrades easier...
    Former-commit-id: e14a799d0cae60c30ce8ad891f9e31f3cffcae9e [formerly 3498d0863c771c72027dacfdcd9cbb9edb672972]
    Former-commit-id: e80e2f8fbf74fa5287342cf01f17ea2a8f1c9c81 [formerly a9f0483b649eff753d62ebbfeaf5c6eb60d97df9]
    Former-commit-id: 31bfac7f8d722543cf22c7ba15df0d16e664aed1

---
+++ aux/mk/upgrade.mk
@@ -0,0 +1,60 @@
+#; -*- mode: Makefile;-*-
+# =========================================================
+# Copyright 2012-2014,  Nuno A. Fonseca (nuno dot fonseca at gmail dot com)
+#
+# This file is part of iRAP.
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with iRAP.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    $Id: 0.1.3 Nuno Fonseca Fri Dec 21 11:56:56 2012$
+# =========================================================
+# Rules for upgrading irap (software and files)
+
+
+ifndef clone_dir
+ $(call error, missing argument clone_dir!)
+else
+ $(info clone_dir=$(clone_dir))
+endif
+
+#
+define main_version=
+$(shell echo $(1)|sed 's/[dp].*$$//')
+endef
+# clone
+irap_clone_dir=$(clone_dir)
+irap_clone_version=$(call main_version,$(shell cat $(clone_dir)/version))
+# current version
+version=$(shell cat $(irap_path)/../version)
+irap_cur_version=$(call main_version,$(version))
+
+
+$(info $(irap_cur_version))
+$(info $(irap_clone_version))
+irap_clone_version=0.5.1
+#
+ifeq ($(irap_cur_version),$(irap_clone_version))
+upgrade_irap: 
+	@echo "Nothing to do"
+else
+upgrade_irap: upgrade_irap_$(irap_cur_version)_to_$(irap_clone_version)
+endif
+
+upgrade_irap_0.5.0_to_0.5.1: 
+	$(irap_clone_dir)/scripts/irap_install.sh -s $(irap_clone_dir) -x R3_packages
+	$(irap_clone_dir)/scripts/irap_install.sh -s $(irap_clone_dir) -u
+	$(irap_clone_dir)/scripts/irap_install.sh -s $(irap_clone_dir) -v
+
+
