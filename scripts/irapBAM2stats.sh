bbdc8e57de94f4ad58a6de6890b4173b7d7bbc6c
Nuno Fonseca <firstname.lastname@gmail.com>
2014-11-20 14:56:38 +0000

    Query effeciently on multiple fields of a BAM file
    Former-commit-id: e7d71b4a23179176bc920ea0c17e145f15074a46 [formerly f3cd1624dd2758e922be99548cb400d7ba6507b8]
    Former-commit-id: 0656ba7892d37d04a3d34c5673be04480f93ea1e [formerly 6685841140eb8c15fc9cf9cb645953263668b365]
    Former-commit-id: 48358f8122f9d5db9436b7809b606829833806a8

---
+++ scripts/irapBAM2stats.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+if [ "$1-" = "-" ]; then
+    echo "Usage: irapBAM2stats2.sh bam_file_name" > /dev/stderr
+    exit 1
+fi
+
+if [ ! -e $1.ridx ]; then
+    echo "Index not found: $1.ridx" > /dev/stderr
+    exit 1
+fi
+
+time sqlite3 $1.ridx <<EOF
+.mode line
+.separator ", "
+
+
+select count(*) as All_entries from bam_index;
+select count(*) as Valid_entries from bam_index where isDuplicate=0 and isNotPassingQualityControls=0;
+select count(*) as Duplicate from bam_index where isDuplicate=1 ;
+select count(*) as Alignments from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 ;
+select count(*) as AlnSpliced from bam_index where isSpliced=1 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1;
+select count(*) as ReadsSpliced from bam_index where isSpliced=1 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and isPrimary=1;
+select count(*) as Paired from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isPaired=1 ;
+select count(*) as Paired_mapped from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and isPaired=1 and isProperPair=1  ;
+select count(*) as Paired_mapped_mate1 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and isPaired=1 and isSecondMateRead=0 ;
+select count(*) as Paired_mapped_mate2 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and isPaired=1 and isSecondMateRead=1 ;
+select count(*) as ReadsMapped from bam_index where  isPrimary=1 and isMapped=1 and  isDuplicate=0 and isNotPassingQualityControls=0;
+select count(*) as Reads from bam_index where  isPrimary=1;
+select count(*) as NM0 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nm=0 ;
+select count(*) as NM1 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nm=1 ;
+select count(*) as NM2 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nm=2 ;
+select count(*) as NM3P from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nm>=3 ;
+select count(*) as plus_strand from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and xs='+' ;
+select count(*) as minus_strand from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and xs='-' ;
+select count(*) as NH1 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh=1 ;
+select count(*) as NHG2 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh>=2 ;
+select count(*) as NHG10 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh>=10 ;
+select count(*) as NHG20 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh>=20 ;
+EOF
+exit 0
