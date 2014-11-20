8dd0a806cc16ee9335b68bc6174fced614053252
Nuno Fonseca <firstname.lastname@gmail.com>
2014-11-20 16:24:27 +0000

    Fix the names of the stats
    Former-commit-id: 8d723f43a7518def982606e858810d719913ee3f [formerly 087bf722647c433563b58da4cfd95ac1c8cc4171]
    Former-commit-id: 679773a2dc455695a930b182566730b330cdfdd2 [formerly 4784435b8ff9a603f95396ac75a41dcb366a92f1]
    Former-commit-id: 87993dd161fcb89519f8b2231184a35487f6305b

--- scripts/irapBAM2stats.sh
+++ scripts/irapBAM2stats.sh
@@ -20,7 +20,7 @@ select count(*) as All_entries from bam_index;
 select count(*) as Valid_entries from bam_index where isDuplicate=0 and isNotPassingQualityControls=0;
 select count(*) as Duplicate from bam_index where isDuplicate=1 ;
 select count(*) as Alignments from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 ;
-select count(*) as AlnSpliced from bam_index where nSpliced>=1 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1;
+select count(*) as Spliced from bam_index where nSpliced>=1 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1;
 select count(*) as ReadsSpliced from bam_index where nSpliced>=1 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and isPrimary=1;
 select count(*) as Paired from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isPaired=1 ;
 select count(*) as Paired_mapped from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and isPaired=1 and isProperPair=1  ;
@@ -38,9 +38,9 @@ select count(*) as NH1 from bam_index where isDuplicate=0 and isNotPassingQualit
 select count(*) as NHGE2 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh>=2 ;
 select count(*) as NHGE10 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh>=10 ;
 select count(*) as NHGE20 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh>=20 ;
-select count(*) as AlnSpliced1 from bam_index where nSpliced=1 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1;
-select count(*) as AlnSpliced2 from bam_index where nSpliced=2 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1;
-select count(*) as AlnSpliced3 from bam_index where nSpliced=3 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1;
-select count(*) as AlnSplicedGE4 from bam_index where nSpliced>3 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1;
+select count(*) as Spliced1 from bam_index where nSpliced=1 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1;
+select count(*) as Spliced2 from bam_index where nSpliced=2 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1;
+select count(*) as Spliced3 from bam_index where nSpliced=3 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1;
+select count(*) as SplicedGE4 from bam_index where nSpliced>3 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1;
 EOF
 exit 0
