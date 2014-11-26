441a1daaeedae93a587585361daab024304bf35a
Nuno Fonseca <firstname.lastname@gmail.com>
2014-11-26 00:00:43 +0000

    WIP
    Former-commit-id: f59f3bd13cffe47461d54e85cd1d32371b501ee0 [formerly 1f44710bd460ad98a42c7ec5297489c93b372423]
    Former-commit-id: 441cfde962614f524aba079f06a981c64bffbff1 [formerly 3a2a57d80d2236912c022a05c0402561ff154ec1]
    Former-commit-id: 2fe71270e50a3611dc6768ea6fe5d1ff21b15731

--- scripts/irapBAM2stats.sh
+++ scripts/irapBAM2stats.sh
@@ -26,7 +26,8 @@ select count(*) as Paired from bam_index where isDuplicate=0 and isNotPassingQua
 select count(*) as Paired_mapped from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and isPaired=1 and isProperPair=1  ;
 select count(*) as Paired_mapped_mate1 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and isPaired=1 and isSecondMateRead=0 ;
 select count(*) as Paired_mapped_mate2 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and isPaired=1 and isSecondMateRead=1 ;
-select count(*) as ReadsUmapped from bam_index where isMapped=0 and  isDuplicate=0 and isNotPassingQualityControls=0;
+select count(*) as ReadsUnmapped from bam_index where isMapped=0 and  isDuplicate=0 and isNotPassingQualityControls=0;
+select count(*) as ReadsMapped from bam_index where isMapped=1 and isPrimary=1 and  isDuplicate=0 and isNotPassingQualityControls=0;
 select count(*) as NM0 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nm=0 ;
 select count(*) as NM1 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nm=1 ;
 select count(*) as NM2 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nm=2 ;
@@ -35,10 +36,10 @@ select count(*) as plus_strand from bam_index where isDuplicate=0 and isNotPassi
 select count(*) as minus_strand from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and xs='-' ;
 select count(*) as UniquelyMappedReads from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh=1 and isPrimary=1;
 select count(*) as MultimapReads from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh>1 and isPrimary=1;
-select count(*) as NH1 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh=1 ;
-select count(*) as NHGE2 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh>=2 ;
-select count(*) as NHGE10 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh>=10 ;
-select count(*) as NHGE20 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh>=20 ;
+select count(*) as NH1 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh=1  and isPrimary=1;
+select count(*) as NHGE2 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh>=2  and isPrimary=1;
+select count(*) as NHGE10 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh>=10  and isPrimary=1;
+select count(*) as NHGE20 from bam_index where isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1 and nh>=20  and isPrimary=1;
 select count(*) as Spliced1 from bam_index where nSpliced=1 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1;
 select count(*) as Spliced2 from bam_index where nSpliced=2 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1;
 select count(*) as Spliced3 from bam_index where nSpliced=3 and isDuplicate=0 and isNotPassingQualityControls=0  and isMapped=1;
