f5b8102f206a08cc587daba0b0a85e62d8a35601
Nuno Fonseca <firstname.lastname@gmail.com>
2015-02-25 23:37:13 +0000

    Unmapped reads should contain RG if provided
    Former-commit-id: 187657d78cd031f6e32da3b525050c7715555850 [formerly 3adbc16f32f1265721a1fa9640829e95528d9d68]
    Former-commit-id: a1f31adc8855afd34e42c1987c82fee0ce234d4f [formerly 8aae9e7b1806af0076630c9e04f5696a794de36a]
    Former-commit-id: 5ade937d260ad14dd33fa41f30a5e95d9f0f24b3

--- aux/mk/irap_map.mk
+++ aux/mk/irap_map.mk
@@ -278,7 +278,7 @@ endef
 define run_tophat2_map=
         $(call tophat_setup_dirs,$(1))
 	irap_map.sh tophat2 tophat2  -p $(max_threads) $(call tophat_seglength_option,$($(1)_rs),$(1)) $(call tophat_qual_option,$($(1)_qual)) $(call tophat_strand_params,$(1)) $(tophat2_map_params) $(call tophat_ins_sd_params,$(1)) -G $(gtf_file_abspath) --tmp-dir $(call lib2bam_folder,$(1))$(1)/tmp -o $(call lib2bam_folder,$(1))$(1) --transcriptome-index $(call tophat2_trans_index_filename,$(file_indexed),$(file_indexed))/  $(if $($(1)_rgid),--rg-id "$($(1)_rgid)" --rg-sample "$($(1)_rgid)")   $(tophat_reference_prefix) $(2) && \
-	$(if $($(1)_rgid),samtools view $(call lib2bam_folder,$(1))$(1)/unmapped.bam | sed "s/$$$$/\tRG:Z:$($(1)_rgid)/" | samtools view -b - > $(call lib2bam_folder,$(1))$(1)/unmapped2.bam && mv $(call lib2bam_folder,$(1))$(1)/unmapped2.bam $(call lib2bam_folder,$(1))$(1)/unmapped.bam  && ) \
+	$(if $($(1)_rgid), $(call addRG2BAM,$(call lib2bam_folder,$(1))$(1)/unmapped.bam,$($(1)_rgid)) && ) \
 	samtools merge - $(call lib2bam_folder,$(1))$(1)/accepted_hits.bam $(call lib2bam_folder,$(1))$(1)/unmapped.bam | \
 	bam_tophat2_fix - - | \
 	samtools sort -m $(SAMTOOLS_SORT_MEM) -T $(call lib2bam_folder,$(1))$(1)/$(1) -o $(call lib2bam_folder,$(1))$(1)/$(1).bam  -  &&\
@@ -286,6 +286,11 @@ define run_tophat2_map=
 	mv $(call lib2bam_folder,$(1))$(1)/$(1).bam $(3)
 endef
 
+# 1-bam
+# 2-rgid
+define addRG2BAM= 
+	samtools view -H $(1) > $(1).header && echo -e "@RG\tID:$(2)" >> $(1).header &&  samtools view $(1) | sed "s/$$$$/\tRG:Z:$(2)/" | samtools view -b - | samtools reheader $(1).header /dev/stdin > $(1)
+endef
 
 ####################################################
 # SMALT
