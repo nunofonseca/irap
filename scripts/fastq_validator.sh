bc1f147e1b7625201de0b6afeb6c219752ca4ce5
Nuno Fonseca <firstname.lastname@gmail.com>
2015-02-26 13:25:44 +0000

    Include support for BAMs...
    Former-commit-id: c1722751bebbad3c563aeb698b0596d2048d62dc [formerly a592ff96360399c4d2bf5a0096e122947944f079]
    Former-commit-id: fc99e5f2a7a79f4f5c4fe43d6e645b4d14dbc3d3 [formerly 43fcff915d8d4572a830cbf8d0330a04ca8ba3c4]
    Former-commit-id: 96b7f9b10339883b423a9577b5f1b89c9061cffe

--- scripts/fastq_validator.sh
+++ scripts/fastq_validator.sh
@@ -8,22 +8,43 @@ function file_extension {
 }
 set -e
 # check extension
-FILES2PROCESS=
-FILES2DELETE=
-for f in $FILES; do
-    ext=`file_extension $f`
-    if [ "-$ext" == "-bz2" ] || [ "-$ext" == "-bzip2" ] ; then
-	echo BZIP file
-	named_pipe=.$f.pipe.fastq
-	mkfifo $named_pipe
-	bunzip2 -k  -c $f > $named_pipe  &
-	FILES2PROCESS="$FILES2PROCESS $named_pipe"
-	FILES2DELETE="$FILES2DELETE $named_pipe"
-    else
-	FILES2PROCESS="$FILES2PROCESS $f"
+ext=`file_extension $1`
+if [ "$ext-" == "bam-" ]; then
+    #hmm, this now validates bams...kind of
+    # samtools version should be 1 or above
+    f=$1    
+    echo "BAM file"
+    # check if the BAM contains unaligned reads
+    echo "Checking for unmapped reads"
+    UN=`samtools view -c -f 4 $f`
+    if [ "$UN-" == "0-" ]; then
+	echo "ERROR: No unaligned reads found in $f." > /dev/stderr
+	exit 1
     fi
-done
-
+    named_pipe=.`basename .$f`.pipe.fastq
+    mkfifo $named_pipe
+    echo "Converting BAM to fastq"
+    samtools bam2fq $f > $named_pipe &
+    
+    FILES2PROCESS=$named_pipe
+    FILES2DELETE=$named_pipe
+else
+    FILES2PROCESS=
+    FILES2DELETE=
+    for f in $FILES; do
+	ext=`file_extension $f`
+	if [ "-$ext" == "-bz2" ] || [ "-$ext" == "-bzip2" ] ; then
+	    echo BZIP file
+	    named_pipe=.`basename .$f`.pipe.fastq
+	    mkfifo $named_pipe
+	    bunzip2 -k  -c $f > $named_pipe  &
+	    FILES2PROCESS="$FILES2PROCESS $named_pipe"
+	    FILES2DELETE="$FILES2DELETE $named_pipe"
+	else
+	    FILES2PROCESS="$FILES2PROCESS $f"
+	fi
+    done
+fi
 fastq_info $FILES2PROCESS
 
 if [ "-$FILES2DELETE" != "-" ]; then
