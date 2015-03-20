d9cd148d76672a7b6b062079ccb1f5985f7fa5f7
Nuno Fonseca <firstname.lastname@gmail.com>
2015-03-20 01:31:59 +0000

    Update version to 0.6.2d7

--- src/bamutils/bam_fix_NH.c
+++ src/bamutils/bam_fix_NH.c
@@ -33,7 +33,7 @@
 
 #define HASHSIZE 7000001
 // Same version as IRAP
-#define VERSION "0.6.1p7"
+#define VERSION "0.6.2d7"
 struct read {
   uint8_t ctr; // how many times a read appears in one alignment
   char *name;// read name
