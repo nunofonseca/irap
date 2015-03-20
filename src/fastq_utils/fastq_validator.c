861588b62a5106ae85d2995795186b57883508a8
Nuno Fonseca <firstname.lastname@gmail.com>
2015-03-20 10:01:33 +0000

    Update version to 0.6.2d8

--- src/fastq_utils/fastq_validator.c
+++ src/fastq_utils/fastq_validator.c
@@ -35,7 +35,7 @@
 // 1MB
 // disable this option if disk access is fast (local disk)
 // enable it for network disks
-#define VERSION "0.6.1p8"
+#define VERSION "0.6.2d8"
 
 #define SEQDISKACCESS 1
 
