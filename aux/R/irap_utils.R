8ca94b3ceefe1ab6d95045a63bbcd5d7e8e542b5
Nuno Fonseca <firstname.lastname@gmail.com>
2015-03-23 13:48:28 +0000

    Update version to 0.6.1p9

--- aux/R/irap_utils.R
+++ aux/R/irap_utils.R
@@ -20,7 +20,7 @@
 # =========================================================
 # TODO: create an object for the conf. information
 
-irap_version<-"0.6.2d8"
+irap_version<-"0.6.1p9"
 
 IRAP.DIR <- Sys.getenv(c("IRAP_DIR"))
 if ( IRAP.DIR == "" ) {
--- src/bamutils/bam_pe_insert.c
+++ src/bamutils/bam_pe_insert.c
@@ -27,7 +27,7 @@
 #include <sam.h>
 #include <kstring.h>      
 
-#define VERSION "0.6.2d8"
+#define VERSION "0.6.1p9"
 #define MAX_INSERT_SIZE 1000
 #define BIN_SIZE 10
      
