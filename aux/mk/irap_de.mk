18b67026561e6de34a02406cd521a7651b9529f0
Nuno Fonseca <firstname.lastname@gmail.com>
2015-04-24 09:37:45 +0100

    Pass the technical replicates info to DESeq

--- aux/mk/irap_de.mk
+++ aux/mk/irap_de.mk
@@ -45,9 +45,8 @@ endef
 # 1=counts file
 # 2=contrast
 # 3=de tsv file
-# TODO: include support for technical replicates
 define run_deseq=
-irap_DE_deseq --tsv $(1) --min $(de_min_count) --contrasts "$(call get_contrast_def,$(2))" --labels "$(call get_contrast_labels,$(2))" --out $(3) $(call get_de_annot) $(call get_de_annot_genes_only)
+irap_DE_deseq --tsv $(1) --min $(de_min_count) --contrasts "$(call get_contrast_def,$(2))" --labels "$(call get_contrast_labels,$(2))" --out $(3) $(call get_de_annot) $(call get_de_annot_genes_only) $(if $(technical.replicates),--tech-rep "$(technical.replicates)")
 endef
 
 #************
@@ -63,7 +62,7 @@ endif
 #
 
 define run_deseq2=
-irap_DE_deseq2 --tsv $(1) --min $(de_min_count) --contrasts "$(call get_contrast_def,$(2))" --labels "$(call get_contrast_labels,$(2))" $(deseq2_params) --out $(3) $(call get_de_annot) $(call get_de_annot_genes_only)
+irap_DE_deseq2 --tsv $(1) --min $(de_min_count) --contrasts "$(call get_contrast_def,$(2))" --labels "$(call get_contrast_labels,$(2))" $(deseq2_params) --out $(3) $(call get_de_annot) $(call get_de_annot_genes_only) $(if $(technical.replicates),--tech-rep "$(technical.replicates)")
 endef
 
 #************
