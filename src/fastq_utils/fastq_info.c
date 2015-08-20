4ac44ae64fe174fa57278596a142816fa9bd7346
Nuno Fonseca <firstname.lastname@gmail.com>
2015-08-20 11:56:38 +0100

    Update version to 0.7.0d3

--- src/fastq_utils/fastq_info.c
+++ src/fastq_utils/fastq_info.c
@@ -36,7 +36,7 @@
 // 1MB
 // disable this option if disk access is fast (local disk)
 // enable it for network disks
-#define VERSION "0.7.0d2"
+#define VERSION "0.7.0d3"
 
 
 #define SEQDISKACCESS 1
