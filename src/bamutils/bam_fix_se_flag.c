67dbb55d99ec5a498113c6d6e6a5739312234486
Nuno Fonseca <firstname.lastname@gmail.com>
2015-09-01 17:49:21 +0100

    Update version to 0.7.0d5

--- src/bamutils/bam_fix_se_flag.c
+++ src/bamutils/bam_fix_se_flag.c
@@ -30,7 +30,7 @@
 #include <string.h>
 #include <assert.h>
 
-#define VERSION "0.7.0p2"
+#define VERSION "0.7.0d5"
 
 
 #define BACKLINE "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
--- src/fastq_utils/fastq_info.c
+++ src/fastq_utils/fastq_info.c
@@ -36,7 +36,7 @@
 // 1MB
 // disable this option if disk access is fast (local disk)
 // enable it for network disks
-#define VERSION "0.7.0p2"
+#define VERSION "0.7.0d5"
 
 
 #define SEQDISKACCESS 1
