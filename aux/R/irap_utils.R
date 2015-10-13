6b5991e50f00e0e00ea1dc1d9e7d37a2a90b71d2
Nuno Fonseca <firstname.lastname@gmail.com>
2015-10-13 15:57:45 +0100

    Update version to 0.7.0p7

--- aux/R/irap_utils.R
+++ aux/R/irap_utils.R
@@ -20,7 +20,7 @@
 # =========================================================
 # TODO: create an object for the conf. information
 
-irap_version<-"0.7.0d10"
+irap_version<-"0.7.0p7"
 
 
 IRAP.DIR <- Sys.getenv(c("IRAP_DIR"))
