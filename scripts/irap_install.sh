eb47031ffcfa91f02c9eea79582174df353c793d
Nuno Fonseca <firstname.lastname@gmail.com>
2015-10-14 16:04:32 +0100

    Update version to 0.7.0p8

--- scripts/irap_install.sh
+++ scripts/irap_install.sh
@@ -24,7 +24,7 @@ install=all
 IRAP_DIR1=
 SRC_DIR=
 
-IRAP_VERSION=0.7.0d10
+IRAP_VERSION=0.7.0p8
 
 
 #
--- src/bamutils/bam_fix_se_flag.c
+++ src/bamutils/bam_fix_se_flag.c
@@ -30,7 +30,7 @@
 #include <string.h>
 #include <assert.h>
 
-#define VERSION "0.7.0d10"
+#define VERSION "0.7.0p8"
 
 
 #define BACKLINE "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
