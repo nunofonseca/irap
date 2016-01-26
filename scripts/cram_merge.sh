be0e3d04924bb645912eb3eaf0870385478dd378
Nuno Fonseca <firstname.lastname@gmail.com>
2016-01-26 08:49:06 +0000

    print errors to stdout. check if the files exist

--- scripts/cram_merge.sh
+++ scripts/cram_merge.sh
@@ -4,12 +4,12 @@
 # * one cram filename per line
 
 if [ "$2-" == "-" ]; then
-    echo "ERROR: usage cram_merge.sh file_with_cram_filenames out_file" > /dev/stderr
+    echo "ERROR: usage cram_merge.sh file_with_cram_filenames out_file" 
     exit 1
 fi
 
 if [ ! -e $1 ]; then
-    echo "ERROR: file $1 not found" > /dev/stderr
+    echo "ERROR: file $1 not found"
     exit 1
 fi
 
@@ -17,6 +17,19 @@ fi
 # assumes no spaces in the path
 cat $1| tr " " "\n" | grep -v "^$" > $2.lst
 
+# check if all files exist
+set -e
+cat $2.lst |  ( while read f; do
+		    echo -n "Checking if $f exists..."
+		    if [ ! -e "$f" ]; then
+			echo 
+			echo "ERROR: file $f not found"
+			exit 1
+		    fi
+		    echo "ok."
+		    done
+)
+set +e
 # get reference
 cram1=`head -n 1 $2.lst`
 echo "Getting reference from $cram1..."
@@ -24,12 +37,12 @@ ref=`samtools view -H $cram1 |grep UR|sed "s/.*UR://g;s/ .*//"|head -n 1`
 echo "Getting reference from $cram1...done."
 echo "ref=$ref"
 if [ "$ref-" == "-" ]; then
-    samtools view -H $cram1 > /dev/stderr
-    echo "ERROR:Unable to find a reference in the cram file?!" > /dev/stderr    
+    samtools view -H $cram1 
+    echo "ERROR:Unable to find a reference in the cram file?!"
     exit 1
 fi
 if [ ! -e "$ref" ]; then
-    echo "Reference file $ref not found" > /dev/stderr
+    echo "Reference file $ref not found"
     exit 1
 fi
 set -e
