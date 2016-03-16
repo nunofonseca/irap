0099115366eb589a9ed4c49ad933217860436a55
Nuno Fonseca <firstname.lastname@gmail.com>
2016-03-16 20:38:14 +0000

    Disable TPM normalization for EA

--- aux/mk/irap_norm.mk
+++ aux/mk/irap_norm.mk
@@ -65,11 +65,13 @@ $(name)/$(mapper)/$(quant_method)/%.transcripts.rpkm.$(quant_method).irap.tsv: $
 $(name)/$(mapper)/$(quant_method)/%.exons.rpkm.$(exon_quant_method).irap.tsv: $(name)/$(mapper)/$(quant_method)/%.exons.raw.$(exon_quant_method).tsv
 	irap_raw2metric --tsv $<  --lengths $(exon_length) --feature exon --metric rpkm --out $@.tmp && mv $@.tmp $@	
 
-
+# TODO: uncomment to unable TPM normalization for Atlas
 ifdef atlas_run
-STAGE3_S_TARGETS+=$(foreach p,$(pe), $(call lib2quant_folder,$(p))$(p).pe.genes.rpkm.$(quant_method).irap.tsv) $(foreach s,$(se), $(call lib2quant_folder,$(s))$(s).se.genes.rpkm.$(quant_method).irap.tsv $(call lib2quant_folder,$(s))$(s).se.genes.tpm.$(quant_method).irap.tsv)
+STAGE3_S_TARGETS+=$(foreach p,$(pe), $(call lib2quant_folder,$(p))$(p).pe.genes.rpkm.$(quant_method).irap.tsv) $(foreach s,$(se), $(call lib2quant_folder,$(s))$(s).se.genes.rpkm.$(quant_method).irap.tsv
+#$(call lib2quant_folder,$(s))$(s).se.genes.tpm.$(quant_method).irap.tsv)
 ifeq ($(exon_quant),y)
-STAGE3_S_TARGETS+=$(foreach p,$(pe), $(call lib2quant_folder,$(p))$(p).pe.exons.rpkm.$(exon_quant_method).irap.tsv) $(foreach s,$(se), $(call lib2quant_folder,$(s))$(s).se.exons.rpkm.$(exon_quant_method).irap.tsv $(call lib2quant_folder,$(s))$(s).se.exons.tpm.$(exon_quant_method).irap.tsv)
+STAGE3_S_TARGETS+=$(foreach p,$(pe), $(call lib2quant_folder,$(p))$(p).pe.exons.rpkm.$(exon_quant_method).irap.tsv) $(foreach s,$(se), $(call lib2quant_folder,$(s))$(s).se.exons.rpkm.$(exon_quant_method).irap.tsv
+#$(call lib2quant_folder,$(s))$(s).se.exons.tpm.$(exon_quant_method).irap.tsv)
 endif
 endif
 
