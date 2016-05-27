0ed94a4358a111dc0c3c2a484c75b4b108289d4e
Nuno Fonseca <firstname.lastname@gmail.com>
2016-05-27 17:35:17 +0100

    Update version to 0.8.1p1

--- aux/R/irap_utils.R
+++ aux/R/irap_utils.R
@@ -20,7 +20,7 @@
 # =========================================================
 # TODO: create an object for the conf. information
 
-irap_version<-"0.8.1p1"
+irap_version<-"0.8.1d2"
 
 
 IRAP.DIR <- Sys.getenv(c("IRAP_DIR"))
