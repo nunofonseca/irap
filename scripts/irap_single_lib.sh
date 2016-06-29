f22c0880a552349e3195e5be7f5f59cb033026b4
Nuno Fonseca <firstname.lastname@gmail.com>
2016-06-29 10:31:19 +0100

    Add wrapper

--- scripts/irap_single_lib.sh
+++ scripts/irap_single_lib.sh
@@ -2,7 +2,7 @@
 # wrapper to irap_single_lib.sh
 set -e
 if which $0 >/dev/null 2>/dev/null; then
-    source `dirname $0`/../irap_setup.sh
+ source `dirname $0`/../irap_setup.sh
 fi
 irap_single_lib $@
 exit 0
