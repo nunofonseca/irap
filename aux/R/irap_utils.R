1041a07141caeccadc4913bdc2ed5a411affdd73
Nuno Fonseca <firstname.lastname@gmail.com>
2017-01-20 17:28:52 +0000

    Update version to 0.8.3

--- aux/R/irap_utils.R
+++ aux/R/irap_utils.R
@@ -20,7 +20,7 @@
 # =========================================================
 # TODO: create an object for the conf. information
 
-irap_version<-"0.8.3"
+irap_version<-"0.8.3d1"
 
 
 IRAP.DIR <- Sys.getenv(c("IRAP_DIR"))
--- src/bamutils/bam_fix_se_flag.c
+++ src/bamutils/bam_fix_se_flag.c
@@ -30,7 +30,7 @@
 #include <string.h>
 #include <assert.h>
 
-#define VERSION "0.8.3"
+#define VERSION "0.8.3d1"
 
 
 #define BACKLINE "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
