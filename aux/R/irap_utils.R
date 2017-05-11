dbf6230c7671a41fe9a2b3b43ecd1766ece847f0
Nuno Fonseca <firstname.lastname@gmail.com>
2017-05-11 15:19:38 +0100

    Update version to 0.8.5p2

--- aux/R/irap_utils.R
+++ aux/R/irap_utils.R
@@ -20,7 +20,7 @@
 # =========================================================
 # TODO: create an object for the conf. information
 
-irap_version<-"0.8.5p2"
+irap_version<-"0.8.5d3"
 
 
 IRAP.DIR <- Sys.getenv(c("IRAP_DIR"))
--- scripts/irap_install.sh
+++ scripts/irap_install.sh
@@ -24,7 +24,7 @@ install=all
 IRAP_DIR1=
 SRC_DIR=
 
-IRAP_VERSION=0.8.5p2
+IRAP_VERSION=0.8.5d3
 
 
 #
