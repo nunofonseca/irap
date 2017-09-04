64b87ece13e2f539e99d69b58a5866993a3bc020
Nuno Fonseca <firstname.lastname@gmail.com>
2017-09-04 09:49:45 +0100

    single cell support

---
+++ aux/mk/irap_qc_stats.mk
@@ -0,0 +1,32 @@
+#; -*- mode: Makefile;-*-
+# =========================================================
+# Copyright 2012-2017,  Nuno A. Fonseca (nuno dot fonseca at gmail dot com)
+#
+# This file is part of iRAP.
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with iRAP.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    $Id: 0.1.3 Nuno Fonseca Wed Dec 26 16:16:19 2012$
+# =========================================================
+
+# QC statistics
+# Generation of a single file with the stats collected from different stages of the analysis
+
+
+
+## When is this executed?
+## End of stage4 (stage3as if ISL mode is enabled)
+
+
