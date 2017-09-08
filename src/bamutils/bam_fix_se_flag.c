cca2edf124af61b559883deaec01d1918d2d7485
Nuno Fonseca <firstname.lastname@gmail.com>
2017-09-08 19:06:58 +0100

    Update version to 0.8.5p4

--- src/bamutils/bam_fix_se_flag.c
+++ src/bamutils/bam_fix_se_flag.c
@@ -30,7 +30,7 @@
 #include <string.h>
 #include <assert.h>
 
-#define VERSION "0.8.5p3"
+#define VERSION "0.8.5p4"
 
 
 #define BACKLINE "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
--- src/bamutils/bam_rm_NH.c
+++ src/bamutils/bam_rm_NH.c
@@ -32,7 +32,7 @@
 #include <assert.h>
 
 // Same version as IRAP
-#define VERSION "0.8.5p3"
+#define VERSION "0.8.5p4"
 
 
 #define BACKLINE "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
