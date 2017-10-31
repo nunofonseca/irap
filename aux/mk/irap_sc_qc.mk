4389952394af47936d92d78a902e4a67140f3ecc
Nuno Fonseca <firstname.lastname@gmail.com>
2017-10-31 00:01:34 +0000

    Initial support for mtx files

--- aux/mk/irap_sc_qc.mk
+++ aux/mk/irap_sc_qc.mk
@@ -26,37 +26,37 @@
 ifeq ($(rnaseq_type),sc)
 
 cell_qc_files=$(name)/$(mapper)/$(quant_method)/genes.raw.$(quant_method).qc.tsv
-ifeq ($(transcript_quant),y)
+ifeq ($(transcript_expr),y)
 cell_qc_files+=$(name)/$(mapper)/$(quant_method)/transcripts.raw.$(quant_method).qc.tsv
 endif
 
 # generate the QC files
 cell_qc: $(cell_qc_files)
 
-irap_cell_qc_params=$(if $(cell_filt_controls),--controls "$(cell_filt_controls)") --min_features $(cell_filt_min_features) $(if $(subst y,,$(cell_filt_outliers)),--outliers,) --max_ERCC $(cell_filt_max_ERCC) --min_counts $(cell_filt_min_cell_expr) --min_expression $(cell_filt_min_expression) 
+irap_cell_qc_params=$(if $(cell_filt_controls),--controls "$(cell_filt_controls)") --min_features $(cell_filt_min_features) $(if $(subst y,,$(cell_filt_outliers)),,--outliers) --max_ERCC $(cell_filt_max_ERCC) --min_counts $(cell_filt_min_cell_expr) --min_expression $(cell_filt_min_expression) 
 
 $(name)/$(mapper)/$(quant_method)/genes.raw.$(quant_method).qc.tsv: $(name)/$(mapper)/$(quant_method)/genes.raw.$(quant_method).$(expr_ext) $(cell_filt_controls)
-	irap_cell_qc --$(expr_format) $< --out $@.tmp $(irap_cell_qc_params) && mv $@.tmp $@
+	irap_cell_qc --$(expr_format) -i $< --out $@.tmp $(irap_cell_qc_params) && mv $@.tmp $@
 
 $(name)/$(mapper)/$(quant_method)/transcripts.raw.$(quant_method).qc.tsv: $(name)/$(mapper)/$(quant_method)/transcripts.raw.$(quant_method).$(expr_ext) $(cell_filt_controls)
-	irap_cell_qc --$(expr_format) $< --out $@.tmp $(irap_cell_qc_params) && mv $@.tmp $@
+	irap_cell_qc --$(expr_format) -i $< --out $@.tmp $(irap_cell_qc_params) && mv $@.tmp $@
 
 
 ## out files
-filtered_expr_matrices=$(name)/$(mapper)/$(quant_method)/genes.raw.filtered.$(quant_method).irap.$(expr_ext)
-ifeq ($(transcript_quant),y)
-filtered_expr_matrices+=$(name)/$(mapper)/$(quant_method)/transcripts.raw.filtered.$(quant_method).irap.$(expr_ext)
+filtered_expr_matrices=$(name)/$(mapper)/$(quant_method)/genes.raw.filtered.$(quant_method).$(expr_ext)
+ifeq ($(transcript_expr),y)
+filtered_expr_matrices+=$(name)/$(mapper)/$(quant_method)/transcripts.raw.filtered.$(quant_method).$(expr_ext)
 endif
 
 # apply the QC filtering
 filter_cells: $(filtered_expr_matrices)
 
 ## filter the expression matrix based on the QC outcome
-$(name)/$(mapper)/$(quant_method)/genes.raw.filtered.$(quant_method).irap.$(expr_ext): $(name)/$(mapper)/$(quant_method)/genes.raw.$(quant_method).$(expr_ext) $(name)/$(mapper)/$(quant_method)/genes.raw.$(quant_method).qc.tsv
+$(name)/$(mapper)/$(quant_method)/genes.raw.filtered.$(quant_method).$(expr_ext): $(name)/$(mapper)/$(quant_method)/genes.raw.$(quant_method).$(expr_ext) $(name)/$(mapper)/$(quant_method)/genes.raw.$(quant_method).qc.tsv
 	irap_filter_cols  -i $< -o $@ --$(expr_format) --qc $(name)/$(mapper)/$(quant_method)/genes.raw.$(quant_method).qc.tsv || (rm -f $@ && exit 1)
 
-$(name)/$(mapper)/$(quant_method)/transcripts.raw.filtered.$(quant_method).irap.tsv: $(name)/$(mapper)/$(quant_method)/transcripts.raw.$(quant_method).$(expr_ext) $(name)/$(mapper)/$(quant_method)/transcripts.raw.$(quant_method).qc.tsv
-	irap_filter_cols  -i $< -o $@ --$(expr_format) --qc $(name)/$(mapper)/$(quant_method)/genes.raw.$(quant_method).qc.tsv  || (rm -f $@ && exit 1)
+$(name)/$(mapper)/$(quant_method)/transcripts.raw.filtered.$(quant_method).$(expr_ext): $(name)/$(mapper)/$(quant_method)/transcripts.raw.$(quant_method).$(expr_ext) $(name)/$(mapper)/$(quant_method)/transcripts.raw.$(quant_method).qc.tsv
+	irap_filter_cols  -i $< -o $@ --$(expr_format) --qc $(name)/$(mapper)/$(quant_method)/transcripts.raw.$(quant_method).qc.tsv  || (rm -f $@ && exit 1)
 
 
 # STAGE4_OFILES+=
