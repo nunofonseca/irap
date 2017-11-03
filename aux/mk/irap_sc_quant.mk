16cb681a5f3db3549e84252b269c6259585679bd
Nuno Fonseca <firstname.lastname@gmail.com>
2017-11-03 09:10:13 +0000

    irap_merge_* - pass a set of files between double quotes

--- aux/mk/irap_sc_quant.mk
+++ aux/mk/irap_sc_quant.mk
@@ -56,7 +56,7 @@ $(call lib2quant_folder,$(1))$(2).transcripts.tpm.kallisto.kallisto.tsv: $(call 
 
 
 $(call lib2quant_folder,$(1))$(2).genes.raw.kallisto.tsv: $(call lib2quant_folder,$(1))$(2).transcripts.raw.kallisto.tsv $(mapTrans2gene)
-	libTSVAggrTransByGene $$< $(mapTrans2gene) $$@.tmp && mv  $$@.tmp $$@
+	libTSVAggrTransByGene -i $$< -m $(mapTrans2gene) -o $$@.tmp && mv  $$@.tmp $$@
 
 
 endef
@@ -104,7 +104,7 @@ endif
 
 # Generate a single matrix
 $(name)/$(mapper)/$(quant_method)/genes.raw.$(quant_method).mtx.gz: $(foreach p,$(pe),$(call lib2quant_folder,$(p))$(p).pe.genes.raw.$(quant_method).mtx.gz) $(foreach s,$(se), $(call lib2quant_folder,$(s))$(s).se.genes.raw.$(quant_method).mtx.gz)
-	( $(call pass_args_stdin,irap_merge_mtx,$@,-o $@ --in $^) ) || ( rm -f $@ ||exit 1)
+	( $(call pass_args_stdin,irap_merge_mtx,$@,-o $@ --in "$^") ) || ( rm -f $@ ||exit 1)
 
 
 endif
@@ -142,12 +142,13 @@ $(foreach l,$(pe),$(eval $(call make-iumi-count-rule,$(l),$(l).pe,transcript,2))
 
 ## 
 $(name)/$(mapper)/$(quant_method)/transcripts.raw.$(quant_method).mtx.gz: $(foreach p,$(pe),$(call lib2quant_folder,$(p))$(p).pe.transcripts.raw.$(quant_method).mtx.gz) $(foreach s,$(se), $(call lib2quant_folder,$(s))$(s).se.transcripts.raw.$(quant_method).mtx.gz)
-	( $(call pass_args_stdin,irap_merge_mtx,$@,-o $@ --in $^) ) || ( rm -f $@ ||exit 1)
+	( $(call pass_args_stdin,irap_merge_mtx,$@,-o $@ --in "$^") ) || ( rm -f $@ ||exit 1)
 endif
 
 # Generate a single matrix
-$(name)/$(mapper)/$(quant_method)/genes.raw.$(quant_method).mtx.gz: $(foreach p,$(pe),$(call lib2quant_folder,$(p))$(p).pe.genes.raw.$(quant_method).mtx.gz) $(foreach s,$(se), $(call lib2quant_folder,$(s))$(s).se.genes.raw.$(quant_method).mtx.gz)
-	( $(call pass_args_stdin,irap_merge_mtx,$@,-o $@ --in "$^") ) || ( rm -f $@ ||exit 1)
+$(name)/$(mapper)/$(quant_method)/genes.raw.$(quant_method).mtx.gz: $(foreach p,$(pe),$(call lib2quant_folder,$(p))$(p).pe.genes.raw.$(quant_method).mtx.gz) $(foreach s,$(se), $(call lib2quant_folder,$(s))$(s).se.genes.raw.$(quant_method).mtx.gz) $(mapTrans2gene)
+	( $(call pass_args_stdin,irap_merge_mtx,$@,--all_feat_col 1 --all_feat_file $(mapTrans2gene) -o $@ --in "$^") ) || ( rm -f $@ ||exit 1)
+
 
 endif
 ## umi_count
