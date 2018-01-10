5f8461634463512583a2bf8a35b25ed08927d972
Nuno Fonseca <firstname.lastname@gmail.com>
2018-01-10 13:06:52 +0000

    Update URL

--- examples/ex_add2contaminationDB2.sh
+++ examples/ex_add2contaminationDB2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Create a ecoli contamination DB to be used with bowtie2
-set -e
-wget ftp://ftp.ncbi.nih.gov/genomes/Bacteria/Escherichia_coli_536_uid58531/NC_008253.fna
-irap_add2contaminationDB -n e_coli -f NC_008253.fna -d ../data/contamination/ -m bowtie2
+set -ea
+wget ftp://ftp.ensemblgenomes.org/pub/release-37/bacteria/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/dna/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.toplevel.fa.gz -O ecoli.fa.gz
+gunzip ecoli.fa.gz
+irap_add2contaminationDB -n e_coli -f ecoli.fa -d ../data/contamination/ -m bowtie2
 echo "IRAP: cont_index=e_coli"
 exit 0
