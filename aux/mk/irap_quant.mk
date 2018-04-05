6ed867ba61363f2d379728b99ccc56bd9668eafb
Nuno Fonseca <firstname.lastname@gmail.com>
2018-04-05 22:23:30 +0100

    fix typo

--- aux/mk/irap_quant.mk
+++ aux/mk/irap_quant.mk
@@ -1307,7 +1307,7 @@ quantification_s: $(STAGE3_S_TARGETS)
 
 #########################################################
 # Take the technical replicates into account if necessary
-techical_replicates_aggr_op=sum
+technical_replicates_aggr_op?=sum
 
 ifeq ($(technical_replicates),)
 ## no technical replicates
@@ -1322,13 +1322,13 @@ $(quant_toplevel_folder)/exons.raw.$(exon_quant_method).$(expr_ext): $(quant_top
 
 else
 $(quant_toplevel_folder)/genes.raw.$(quant_method).$(expr_ext):$(quant_toplevel_folder)/genes.uraw.$(quant_method).$(expr_ext)
-	$(call pass_args_stdin,irap_aggr_cols,$@.tmp, --tsv=$<  --labels "$(technical_replicates_labels)"  --groups "$(technical_replicates)" --out $@.tmp --op $(techical_replicates_aggr_op)) && mv $@.tmp $@
+	$(call pass_args_stdin,irap_aggr_cols,$@.tmp, --tsv=$<  --labels "$(technical_replicates_labels)"  --groups "$(technical_replicates)" --out $@.tmp --op $(technical_replicates_aggr_op)) && mv $@.tmp $@
 
 $(quant_toplevel_folder)/transcripts.raw.$(quant_method).$(expr_ext): $(quant_toplevel_folder)/transcripts.uraw.$(quant_method).$(expr_ext)
-	$(call pass_args_stdin,irap_aggr_cols,$@.tmp, --tsv=$<  --labels "$(technical_replicates_labels)"  --groups "$(technical_replicates)" --out $@.tmp --op $(techical_replicates_aggr_op)) && mv $@.tmp $@
+	$(call pass_args_stdin,irap_aggr_cols,$@.tmp, --tsv=$<  --labels "$(technical_replicates_labels)"  --groups "$(technical_replicates)" --out $@.tmp --op $(technical_replicates_aggr_op)) && mv $@.tmp $@
 
 $(quant_toplevel_folder)/exons.raw.$(exon_quant_method).$(expr_ext): $(quant_toplevel_folder)/exons.uraw.$(exon_quant_method).$(expr_ext)
-	$(call pass_args_stdin,irap_aggr_cols,$@.tmp, --tsv=$<  --labels "$(technical_replicates_labels)"  --groups "$(technical_replicates)" --out $@.tmp --op $(techical_replicates_aggr_op)) && mv $@.tmp $@
+	$(call pass_args_stdin,irap_aggr_cols,$@.tmp, --tsv=$<  --labels "$(technical_replicates_labels)"  --groups "$(technical_replicates)" --out $@.tmp --op $(technical_replicates_aggr_op)) && mv $@.tmp $@
 
 endif
 
