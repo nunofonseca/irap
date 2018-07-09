d213be72ab95182aaef7b1adf9235edac117474f
Jonathan Manning <jmanning@ebi.ac.uk>
2018-07-09 22:15:58 +0100

    Fix issue due to sudo'd users not having write permission to /dev/stderr

--- scripts/irap_control.sh
+++ scripts/irap_control.sh
@@ -194,7 +194,7 @@ function is_all_done {
     conf=$(id2conf_fp $id)
 
     pushd $run_dir >/dev/null
-    irap_sc conf=$(basename $conf) atlas_bundle > /dev/stderr
+    irap_sc conf=$(basename $conf) atlas_bundle 1>&2
     let ret=$?
     ##echo ret=$ret > /dev/stderr
     popd >/dev/null
@@ -383,7 +383,7 @@ for f in $SDRF_FILES; do
 #	complete)
 	run_complete)
 	    s=`is_all_done $id $run_dir`
-	    echo "s=$s" > /dev/stderr
+	    echo "s=$s" 1>&2
 	    if [ "$s" == "y" ]; then
 		# set as all done
 		ddd=$(readlink -f `rundir2bundle_dir $run_dir`)
