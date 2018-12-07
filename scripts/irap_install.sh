018939bdacf85b43b371259279ecbf6db0ae7112
Nuno Fonseca <firstname.lastname@gmail.com>
2018-12-07 02:10:08 +0000

    Upgrade fastq_utils

--- scripts/irap_install.sh
+++ scripts/irap_install.sh
@@ -1064,9 +1064,9 @@ function samtools_install {
     pushd samtools-${SAMTOOLS_VERSION}
     make -j $J
     make -j $J razip
-    mkdir -p $BIN_DIR/samtools0.x/bin
-    cp samtools razip bcftools/vcfutils.pl bcftools/bcftools $BIN_DIR/samtools0.x/bin
-    mkdir -p $BIN_DIR/samtools0.x/lib
+    mkdir -p $BIN_DIR/bin/samtools0.x/bin
+    cp samtools razip bcftools/vcfutils.pl bcftools/bcftools $BIN_DIR/bin/samtools0.x/bin
+    mkdir -p $BIN_DIR/bin/samtools0.x/lib
     mkdir -p $BIN_DIR/samtools0.x/include/bcftools
     cp *.h $BIN_DIR/samtools0.x/include
     cp bcftools/*.h $BIN_DIR/samtools0.x/include/bcftools
