5bb685dd81814fe925bcc679edae6212647a4ae1
Nuno Fonseca <firstname.lastname@gmail.com>
2018-12-10 10:55:45 +0000

    Fix typo and only install hdf5r_1.0.0 if Seurat fails to install

--- scripts/install_R_packages.R
+++ scripts/install_R_packages.R
@@ -67,10 +67,6 @@ cat("Installing packages:\n")
 biocLite("Rcpp",ask=FALSE, suppressUpdates=FALSE)
 install.packages("http://cran.rstudio.com/src/contrib/00Archive/RcppArmadillo/RcppArmadillo_0.6.600.4.0.tar.gz")
 
-# hdf5r requires a version of hdf5 lib too recent
-biocLite("https://stat.ethz.ch/CRAN/src/contrib/00Archive/hdf5r/hdf5r_1.0.0.tar.gz",suppressUpdates=c("^RcppArmadillo"))
-B
-# 
 
 # Include the main packages in irap_gen_version_mk.sh
 packages2install<-c("intervals","gclus",'R2HTML',"agricolae","bit64",
@@ -85,8 +81,17 @@ packages2install<-c("intervals","gclus",'R2HTML',"agricolae","bit64",
                     "scran", "Seurat")
 for (p in packages2install ) {
   cat("PACKAGE:",p,"\n")
-  biocLite(p,ask=FALSE, suppressUpdates=c("^RcppArmadillo","^hdf5r"))
+  biocLite(p,ask=FALSE, suppressUpdates=c("^RcppArmadillo"))
 }
+
+if( ! "Seurat" %in% rownames(installed.packages())) {
+    ## 
+    # hdf5r requires a version of hdf5 lib too recent in some distros
+    biocLite("https://stat.ethz.ch/CRAN/src/contrib/00Archive/hdf5r/hdf5r_1.0.0.tar.gz",suppressUpdates=c("^RcppArmadillo"))
+    biocLite(p,ask=FALSE, suppressUpdates=c("^RcppArmadillo","^Seurat"))
+}
+# 
+
 # http://bioconductor.org/packages/2.13/data/annotation/src/contrib/GO.db_2.10.1.tar.gz fails to install
 #biocLite('GO.db',ask=FALSE, suppressUpdates=TRUE)
 #biocLite("topGO",ask=FALSE, suppressUpdates=TRUE)
