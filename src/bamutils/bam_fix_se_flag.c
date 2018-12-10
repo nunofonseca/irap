33680c5151d9c27593dea62649754cc5cfd13fb2
Nuno Fonseca <firstname.lastname@gmail.com>
2018-12-10 14:26:41 +0000

    Update version to 1.0.6b

--- src/bamutils/bam_fix_se_flag.c
+++ src/bamutils/bam_fix_se_flag.c
@@ -30,7 +30,7 @@
 #include <string.h>
 #include <assert.h>
 
-#define VERSION "1.0.6b"
+#define VERSION "1.0.6bd"
 
 
 #define BACKLINE "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
--- src/bamutils/bam_tophat2_fix.c
+++ src/bamutils/bam_tophat2_fix.c
@@ -27,7 +27,7 @@
 #include <sam.h>
 #include <kstring.h>      
 
-#define VERSION "1.0.6b"
+#define VERSION "1.0.6bd"
 #define MAX_INSERT_SIZE 1000
 #define BIN_SIZE 10
      
